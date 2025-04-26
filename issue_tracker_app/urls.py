56902737fe99a14f2458c4ec05c2f81987787401
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-04-26 18:10:12 +0530

    Delete issue_tracker_app directory

--- issue_tracker_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('users', views.user, name="users_view"),
-    path('login', views.login, name="login_view")
-]
